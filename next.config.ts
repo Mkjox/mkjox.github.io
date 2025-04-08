c4c3527f7d436ed4f476348b92af3c4d3ccc73ea
jox <zincir_890@hotmail.com>
2025-04-08 13:47:25 +0300

    Trying it again

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/mkjox.github.io",
+  // basePath: "/mkjox.github.io",
   images: {
     unoptimized: true,
   },
